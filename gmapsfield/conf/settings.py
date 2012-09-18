6b081b1f59113b4f62a8bcfed3afa4049bcc259c
Laoska Benyasca <laoska@guegue.net>
2012-09-18 17:12:17 -0600

    cambiando la version de jquery

--- gmapsfield/conf/settings.py
+++ gmapsfield/conf/settings.py
@@ -2,7 +2,7 @@ from django.conf import settings
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
-TEMPLATE_LOADERS = ( 
+TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 )
@@ -10,20 +10,20 @@ MEDIA_ROOT = 'public'
 MEDIA_URL = '/gmaps/media/'
 MIDDLEWARE_CLASSES = ( 'django.middleware.common.CommonMiddleware', )
 TEMPLATE_DIRS = ( 'templates', )
-INSTALLED_APPS = ( 
+INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'gmapsfield.templatetags',
 )
 
-# Alias 
+# Alias
 STATIC_URL = settings.STATIC_URL
 # Custom settings
-GMAP_JQUERY = getattr(settings, 'GMAP_JQUERY', 'http://code.jquery.com/jquery-1.7.2.min.js')
+GMAP_JQUERY = getattr(settings, 'GMAP_JQUERY', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js')
 
-GMAP_JQUERY_UI = getattr(settings, 'GMAP_JQUERY_UI', 
+GMAP_JQUERY_UI = getattr(settings, 'GMAP_JQUERY_UI',
                          'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/jquery-ui.min.js')
 
-GMAP_JQUERY_UI_CSS = getattr(settings, 'GMAP_JQUERY_UI_CSS', 
+GMAP_JQUERY_UI_CSS = getattr(settings, 'GMAP_JQUERY_UI_CSS',
                              'http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.9/themes/smoothness/jquery-ui.css')
 
 GMAP_API = getattr(settings, 'GMAP_API', 'https://maps.google.com/maps/api/js?sensor=false')
